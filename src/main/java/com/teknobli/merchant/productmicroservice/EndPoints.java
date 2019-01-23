be4e392de99816266678662f8528b2b5e46a35bc
rishi <rishisharma7361@gmail.com>
2019-01-23 21:46:38 +0530

    endpoints

--- src/main/java/com/teknobli/merchant/productmicroservice/EndPoints.java
+++ src/main/java/com/teknobli/merchant/productmicroservice/EndPoints.java
@@ -2,7 +2,7 @@ package com.teknobli.merchant.productmicroservice;
 
 public class EndPoints {
 
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL = "https://teknobli-product.herokuapp.com/";
 
     public static final String UPDATE_RATING = "/merchant/updateRating/";
 
